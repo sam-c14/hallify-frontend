c02f1019b264e9a3a91d22fd6848aae236eb9a45
sam-c14 <samuelaffah14@gmail.com>
2025-03-05 13:53:22 +0100

    Update statics

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -2,7 +2,7 @@ export function setupMenuToggle(container) {
   // Ensure navbar.css is loaded
   const style = document.createElement("link");
   style.rel = "stylesheet";
-  style.href = "./assets/css/components/navbar.css";
+  style.href = "./assets/css/components/navbar-footer.css";
   document.head.appendChild(style);
 
   // Menu toggle logic
