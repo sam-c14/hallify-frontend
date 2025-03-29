796a073c5874dc0d97af4d78cbfb226d59d20ee5
sam-c14 <samuelaffah14@gmail.com>
2025-03-29 11:27:59 +0100

    Profile dropdown adjusted

--- src/components/ProfileDropdown.jsx
+++ src/components/ProfileDropdown.jsx
@@ -30,8 +30,8 @@ const ProfileDropdown = ({ user, logoutUser }) => {
       </button>
 
       {showDropdown && (
-        <div className="absolute min-w-52 top-5 -left-20 bg-white shadow-md rounded-lg">
-          <h4 className="font-inter sm:text-sm text-xs font-semibold mb-4">
+        <div className="absolute min-w-52 top-12  -left-20 bg-white shadow-md rounded-lg">
+          <h4 className="font-inter sm:text-sm text-xs font-semibold mb-4 pl-3">
             Welcome {user.name}{" "}
           </h4>
           <button
